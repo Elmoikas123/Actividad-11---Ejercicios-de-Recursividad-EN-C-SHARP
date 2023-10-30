0a30f0cb0b1cd7e5e8d2bfeff2bcb32010281a5d
mollo <mollo@PCMOIKAS>
2023-10-29 22:10:33 -0600

    Agregar archivos de proyecto.

---
+++ Actividad 11 - Ejercicios de Recursividad EN C SHARP/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TABLA_DE_MULTIPLICAR_CON_RECURSION
+{
+    class Program
+    {
+        static void ImprimirTabla(int numero, int multiplicador)
+        {
+            if (multiplicador <= 12)
+            {
+                int resultado = numero * multiplicador;
+                Console.WriteLine($"{numero} * {multiplicador} = {resultado}");
+                ImprimirTabla(numero, multiplicador + 1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int NUM;
+            string linea;
+            Console.Write("DIGITE NÚMERO: ");
+            linea = Console.ReadLine();
+            NUM = int.Parse(linea);
+
+            ImprimirTabla(NUM, 1);
+
+            Console.Write("Pulse una tecla: ");
+            Console.ReadLine();
+        }
+    }
+}
